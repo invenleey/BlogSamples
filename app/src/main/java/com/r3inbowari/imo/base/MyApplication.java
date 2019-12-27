8f6e0cb28d2e860915b733fab6dfa55f61db03f2
r3inbowari <knyn@qq.com>
2019-12-28 05:36:26 +0800

    imomoe player update

---
+++ app/src/main/java/com/r3inbowari/imo/base/MyApplication.java
@@ -0,0 +1,29 @@
+package com.r3inbowari.imo.base;
+
+import android.app.Application;
+
+import com.r3inbowari.imo.imomoeAPI.ImomoeSearch;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tanbiheng on 2018/1/24.
+ */
+
+public class MyApplication extends Application {
+
+    private static MyApplication INSTANCE = null;
+
+    public static ArrayList<ImomoeSearch> arrayList = new ArrayList<>();
+
+    public static Application getInstance(){
+        return INSTANCE;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        arrayList.add(new ImomoeSearch());
+        INSTANCE = this;
+    }
+}
